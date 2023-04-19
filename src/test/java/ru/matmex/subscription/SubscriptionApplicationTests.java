a1dc48b600bfec933397b822940eb5f8a68f9b0c
RomanNechaev <romenkagg@gmail.com>
2023-04-19 15:29:29 +0500

    added gitignore

---
+++ src/test/java/ru/matmex/subscription/SubscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.matmex.subscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
