c153665310c6f1a20950479e59d807320d1de177
RomanNechaev <romenkagg@gmail.com>
2023-04-21 22:28:21 +0500

    изменил отображение полей и методов

--- src/main/java/ru/matmex/subscription/entities/Category.java
+++ src/main/java/ru/matmex/subscription/entities/Category.java
@@ -8,7 +8,23 @@ import java.util.List;
 @Entity
 @Table(name = "Categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Subscription> subscriptions;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
     public Category() {
+    }
+
+    public Category(String name, List<Subscription> subscriptions, User user) {
+        this.name = name;
+        this.subscriptions = subscriptions;
+        this.user = user;
     }
 
     public String getName() {
@@ -27,18 +43,11 @@ public class Category {
         this.subscriptions = subscriptions;
     }
 
-    public Category(String name, List<Subscription> subscriptions, User user) {
-        this.name = name;
-        this.subscriptions = subscriptions;
-        this.user = user;
+    public Long getId() {
+        return id;
     }
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
-    private List<Subscription> subscriptions;
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
+    public User getUser() {
+        return user;
+    }
 
 }
