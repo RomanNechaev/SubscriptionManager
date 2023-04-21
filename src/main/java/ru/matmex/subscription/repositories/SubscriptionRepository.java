f9c4ef3360792bd9d855d877ef5e07f051fb546f
RomanNechaev <romenkagg@gmail.com>
2023-04-21 13:11:29 +0500

    добавил репозиторий для подписки

--- src/main/java/ru/matmex/subscription/repositories/SubscriptionRepository.java
+++ src/main/java/ru/matmex/subscription/repositories/SubscriptionRepository.java
@@ -2,7 +2,12 @@ package ru.matmex.subscription.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import ru.matmex.subscription.entities.Subscription;
+import ru.matmex.subscription.entities.User;
 
-public interface SubscriptionRepository extends CrudRepository<Subscription,Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface SubscriptionRepository extends CrudRepository<Subscription, Long> {
+    Optional<Subscription> findSubscriptionById(Long id);
+    Optional<List<Subscription>> findSubscriptionByUser(User user);
 }
