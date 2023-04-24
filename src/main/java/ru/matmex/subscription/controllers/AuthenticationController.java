9e266919392836d73373d63a12dd43d13b13141d
RomanNechaev <romenkagg@gmail.com>
2023-04-24 20:59:42 +0500

    контроллер для аутентификация

---
+++ src/main/java/ru/matmex/subscription/controllers/AuthenticationController.java
@@ -0,0 +1,11 @@
+package ru.matmex.subscription.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+@CrossOrigin
+public class AuthenticationController {
+
+}
