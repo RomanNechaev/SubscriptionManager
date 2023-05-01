585ab170aff9f95d503dd78602e696bb736425c3
RomanNechaev <romenkagg@gmail.com>
2023-05-01 17:13:59 +0500

    добавил контроллер для авторизации

--- src/main/java/ru/matmex/subscription/controllers/AuthenticationController.java
+++ src/main/java/ru/matmex/subscription/controllers/AuthenticationController.java
@@ -1,11 +1,25 @@
 package ru.matmex.subscription.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
+import ru.matmex.subscription.models.security.AuthRequest;
+import ru.matmex.subscription.models.security.AuthResponse;
+import ru.matmex.subscription.services.AuthenticationService;
 
 @Controller
 @CrossOrigin
 public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    @Autowired
+    public AuthenticationController(AuthenticationService authenticationService) {
 
+        this.authenticationService = authenticationService;
+    }
+    @PostMapping("auth/login")
+    public ResponseEntity<AuthResponse> authUser(@RequestBody AuthRequest authRequest) {
+        return ResponseEntity.ok(authenticationService.authUser(authRequest));
+    }
 }
