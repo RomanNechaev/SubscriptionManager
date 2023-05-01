5c443c08d548f44e28485406ac23c00eddb09544
RomanNechaev <romenkagg@gmail.com>
2023-05-01 17:18:05 +0500

    реализовал контроллер для регистрации

--- src/main/java/ru/matmex/subscription/controllers/RegistrationController.java
+++ src/main/java/ru/matmex/subscription/controllers/RegistrationController.java
@@ -1,4 +1,24 @@
 package ru.matmex.subscription.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.matmex.subscription.models.user.UserModel;
+import ru.matmex.subscription.models.user.UserRegistrationModel;
+import ru.matmex.subscription.services.UserService;
+
+@Controller
+@CrossOrigin
 public class RegistrationController {
+    private final UserService userService;
+
+    @Autowired
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping("/registration")
+    public ResponseEntity<UserModel> registration(@RequestBody UserRegistrationModel userRegistrationModel) {
+        return ResponseEntity.ok(userService.adduser(userRegistrationModel));
+    }
 }
