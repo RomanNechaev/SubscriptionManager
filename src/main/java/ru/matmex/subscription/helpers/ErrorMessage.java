577b9b177414e5364a753c7f478077090b11af9f
RomanNechaev <romenkagg@gmail.com>
2023-05-01 17:16:46 +0500

    переделал pojo классы на рекорды

--- src/main/java/ru/matmex/subscription/helpers/ErrorMessage.java
+++ src/main/java/ru/matmex/subscription/helpers/ErrorMessage.java
@@ -1,16 +1,4 @@
 package ru.matmex.subscription.helpers;
 
-public class ErrorMessage {
-    private String message;
-
-    public ErrorMessage(String message) {
-        this.message = message;
-    }
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
+public record ErrorMessage(String message) {
 }
