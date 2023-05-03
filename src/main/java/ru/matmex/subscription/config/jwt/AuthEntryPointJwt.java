3dfa23d16ac6c30efeda0e53cf13def45423d976
RomanNechaev <romenkagg@gmail.com>
2023-05-03 19:05:25 +0500

    удалил ненужное исключение

--- src/main/java/ru/matmex/subscription/config/jwt/AuthEntryPointJwt.java
+++ src/main/java/ru/matmex/subscription/config/jwt/AuthEntryPointJwt.java
@@ -17,7 +17,7 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
+                         AuthenticationException authException) throws IOException {
         System.err.println(authException.getMessage()); //TODO toLogger
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
